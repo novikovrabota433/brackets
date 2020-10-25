a3c6e29076c4372f5dbbad20e0f0b7c52d89316c
novikovrabota433 <32838534+novikovrabota433@users.noreply.github.com>
2020-10-25 14:56:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+
+    let pairsBrackets = [];
+
+    let lengthStr = str.length;
+
+
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+
+        pairsBrackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+
+    }
+
+
+
+    while (true) {
+
+        for (let i = 0; i < pairsBrackets.length; i++) {
+
+            str = str.replace(pairsBrackets[i], '');
+
+        }
+
+
+
+        if (str.length == 0) return true;
+
+        if (lengthStr == str.length) return false;
+
+
+
+        lengthStr = str.length;
+
+    }
+
 }
